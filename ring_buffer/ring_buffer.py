567dd076192a57908c5422efbfeb67c8639b30e7
Connor Sullivan <crsullivan@protonmail.com>
2020-03-27 10:37:28 -0700

    MVP
    going to go through and add some comments

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -9,24 +9,24 @@ class RingBuffer:
 
     def append(self, item):
         if len(self.storage) < self.capacity:
-            self.storage.add_to_tail(item)
-            current = self.storage.head
-        elif len(self.storage) == self.capacity and current == self.storage.head:
-            self.storage.remove_from_head()
-            self.storage.add_to_head(item)
-            current = self.storage.next
-        elif len(self.storage) == self.capacity:
-            gone = current
-            current = self.storage.next
-            self.storage.insert_before(gone)
-            delete
-
+            if self.storage.length == (self.capacity -1):
+                self.current = self.storage.head
+                self.storage.add_to_tail(item)
+            else:
+                self.storage.add_to_tail(item)
+        elif self.storage.length == self.capacity:
+            self.storage.tail.next = self.storage.head
+            print("PREV", self.current.value)
+            self.current.value = item
+            print("NEW", self.current.value)
+            self.current = self.current.next
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
         mover = self.storage.head
         list_buffer_contents.append(mover.value)
+        print(self.storage.tail.value)
         while mover is not self.storage.tail:
             mover = mover.next
             list_buffer_contents.append(mover.value)
